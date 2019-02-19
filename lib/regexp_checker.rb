d248fa85e4a6197fcc1ca72c2391a69d0f64734f
kiyotan815 <kyt.ibaragi0815@gmail.com>
2019-02-19 12:50:12 +0900

    regexp-checker

---
+++ lib/regexp_checker.rb
@@ -0,0 +1,17 @@
+print 'Text?: '
+text = gets.chomp
+begin
+  print 'Pattern?: '
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue RegexpError => e
+  puts "invalid pattern: #{e.message}"
+  retry
+end
+
+matches = text.scan(regexp)
+if matches.size > 0
+  puts "Matched: #{matches.join(',')}"
+else
+  puts "Nothing Matched"
+end
